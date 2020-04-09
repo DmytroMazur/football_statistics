5b0e801be9838493647b71ab17c4aa87c0bd7797
Dima <dmazur@roi-up.es>
2020-04-09 18:21:59 +0200

    theme frontend

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,210 @@
+@extends('frontend/index')
+
+@section('content')
+<div id="fh5co-wrapper">
+    <div id="fh5co-page">
+    <div id="fh5co-header">
+        <header id="fh5co-header-section">
+            <div class="container">
+                <div class="nav-header">
+                    <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+                    <h1 id="fh5co-logo"><a href="index.html">Listing</a></h1>
+                    <!-- START #fh5co-menu-wrap -->
+                    <nav id="fh5co-menu-wrap" role="navigation">
+                        <ul class="sf-menu" id="fh5co-primary-menu">
+                            <li class="active">
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li>
+                                <a href="listing.html" class="fh5co-sub-ddown">Listing</a>
+                                <ul class="fh5co-sub-menu">
+                                    <li><a href="http://freehtml5.co/preview/?item=build-free-html5-bootstrap-template" target="_blank">Build</a></li>
+                                    <li><a href="http://freehtml5.co/preview/?item=work-free-html5-template-bootstrap" target="_blank">Work</a></li>
+                                    <li><a href="http://freehtml5.co/preview/?item=light-free-html5-template-bootstrap" target="_blank">Light</a></li>
+                                    <li><a href="http://freehtml5.co/preview/?item=relic-free-html5-template-using-bootstrap" target="_blank">Relic</a></li>
+                                    <li><a href="http://freehtml5.co/preview/?item=display-free-html5-template-using-bootstrap" target="_blank">Display</a></li>
+                                    <li><a href="http://freehtml5.co/preview/?item=sprint-free-html5-template-bootstrap" target="_blank">Sprint</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="fh5co-sub-ddown">Dropdown</a>
+                                 <ul class="fh5co-sub-menu">
+                                     <li><a href="left-sidebar.html">Web Development</a></li>
+                                     <li><a href="right-sidebar.html">Branding &amp; Identity</a></li>
+                                    <li>
+                                        <a href="#" class="fh5co-sub-ddown">Free HTML5</a>
+                                        <ul class="fh5co-sub-menu">
+                                            <li><a href="http://freehtml5.co/preview/?item=build-free-html5-bootstrap-template" target="_blank">Build</a></li>
+                                            <li><a href="http://freehtml5.co/preview/?item=work-free-html5-template-bootstrap" target="_blank">Work</a></li>
+                                            <li><a href="http://freehtml5.co/preview/?item=light-free-html5-template-bootstrap" target="_blank">Light</a></li>
+                                            <li><a href="http://freehtml5.co/preview/?item=relic-free-html5-template-using-bootstrap" target="_blank">Relic</a></li>
+                                            <li><a href="http://freehtml5.co/preview/?item=display-free-html5-template-using-bootstrap" target="_blank">Display</a></li>
+                                            <li><a href="http://freehtml5.co/preview/?item=sprint-free-html5-template-bootstrap" target="_blank">Sprint</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">UI Animation</a></li>
+                                    <li><a href="#">Copywriting</a></li>
+                                    <li><a href="#">Photography</a></li> 
+                                </ul>
+                            </li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        
+    </div>
+    
+    
+    <div class="fh5co-hero">
+        <div class="fh5co-overlay"></div>
+        <div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url({{ asset('upload/frontend/cover_bg_1.jpg') }});">
+            <div class="desc animate-box">
+                <h2>Travel Around The World.</h2>
+                <!-- <span>Lovely Crafted by <a href="http://frehtml5.co/" target="_blank" class="fh5co-site-name">FREEHTML5.co</a></span> -->
+                <span><a class="btn btn-primary btn-lg" href="#">Get Started</a></span>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="fh5co-listing">
+        <div class="container">
+            <div class="row">
+                @foreach ($countries as $country)
+                    <div class="col-md-4 col-sm-4 fh5co-item-wrap">
+                        <a class="fh5co-listing-item">
+                            
+                            <img src="{{ asset('upload/frontend/img-1.jpg') }}" alt="Free HTML5 Bootstrap Template by FreeHTML5.co" class="img-responsive">
+                            <div class="fh5co-listing-copy">
+                            <h2>{{ $country->country_name }}</h2>
+                                <span class="icon">
+                                    <i class="icon-chevron-right"></i>
+                                </span>
+                            </div>
+                        </a>
+                    </div>   
+                @endforeach
+            {{-- <div class="col-md-4 col-sm-4 fh5co-item-wrap">
+                    <a class="fh5co-listing-item">
+                        
+                        <img src="{{ asset('upload/frontend/img-1.jpg') }}" alt="Free HTML5 Bootstrap Template by FreeHTML5.co" class="img-responsive">
+                        <div class="fh5co-listing-copy">
+                            <h2>Paris</h2>
+                            <span class="icon">
+                                <i class="icon-chevron-right"></i>
+                            </span>
+                        </div>
+                    </a>
+                </div> --}}
+            {{-- <div class="col-md-4 col-sm-4 fh5co-item-wrap">
+                    <a class="fh5co-listing-item">
+                        <img src="{{ asset('upload/frontend/img-2.jpg') }}" alt="Free HTML5 Bootstrap Template by FreeHTML5.co" class="img-responsive">
+                        <div class="fh5co-listing-copy">
+                            <h2>New York</h2>
+                            <span class="icon">
+                                <i class="icon-chevron-right"></i>
+                            </span>
+                        </div>
+                    </a>
+                </div> --}}    
+            {{-- <div class="col-md-4 col-sm-4 fh5co-item-wrap">
+                    <a class="fh5co-listing-item">
+                        <img src="{{ asset('upload/frontend/img-3.jpg') }}" alt="Free HTML5 Bootstrap Template by FreeHTML5.co" class="img-responsive">
+                        <div class="fh5co-listing-copy">
+                            <h2>London</h2>
+                            <span class="icon">
+                                <i class="icon-chevron-right"></i>
+                            </span>
+                        </div>
+                    </a>
+                </div> --}}    
+                <!-- END 3 row -->
+
+            {{--
+                <div class="col-md-4 col-sm-4 fh5co-item-wrap">
+                    <a class="fh5co-listing-item">
+                        <img src="{{ asset('upload/frontend/img-4.jpg') }}" alt="Free HTML5 Bootstrap Template by FreeHTML5.co" class="img-responsive">
+                        <div class="fh5co-listing-copy">
+                            <h2>Amsterdam</h2>
+                            <span class="icon">
+                                <i class="icon-chevron-right"></i>
+                            </span>
+                        </div>
+                    </a>
+                </div> --}}    
+                
+                <!-- END 3 row -->
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="fh5co-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 fh5co-news">
+                    <h3>News</h3>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <span class="fh5co-date">Sep. 10, 2016</span>
+                                <h3>Newly done Bridge of London</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus reprehenderit!</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="fh5co-date">Sep. 10, 2016</span>
+                                <h3>Newly done Bridge of London</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus reprehenderit!</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="fh5co-date">Sep. 10, 2016</span>
+                                <h3>Newly done Bridge of London</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus reprehenderit!</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-6 fh5co-testimonial">
+                    <img src="{{ asset('upload/frontend/cover_bg_1.jpg') }}" alt="Free HTML5 Bootstrap Template by FreeHTML5.co" class="img-responsive mb20">
+                    <img src="{{ asset('upload/frontend/cover_bg_1.jpg') }}" alt="Free HTML5 Bootstrap Template by FreeHTML5.co" class="img-responsive">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <footer>
+        <div id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3 text-center">
+                        <p class="fh5co-social-icons">
+                            <a href="#"><i class="icon-twitter2"></i></a>
+                            <a href="#"><i class="icon-facebook2"></i></a>
+                            <a href="#"><i class="icon-instagram"></i></a>
+                            <a href="#"><i class="icon-dribbble2"></i></a>
+                            <a href="#"><i class="icon-youtube"></i></a>
+                        </p>
+                        <p>Copyright 2016 Free Html5 <a href="#">Listing</a>. All Rights Reserved. <br>Made with <i class="icon-heart3"></i> by <a href="http://freehtml5.co/" target="_blank">Freehtml5.co</a> / Demo Images: <a href="https://unsplash.com/" target="_blank">Unsplash</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+</div>
+<!-- END fh5co-page -->
+
+</div>
+<!-- END fh5co-wrapper -->
+
+<!-- jQuery -->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'frontend\HomeController@index')->name('home');
 
 Route::group(['prefix'=>'admin',  'middleware' => 'auth'], function(){
     

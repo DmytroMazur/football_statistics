adb7284885d045066af7e645b11371d522eb48a8
Dima <dmazur@roi-up.es>
2020-05-04 17:53:27 +0200

    separate routes

--- travel_v1/app/Http/Controllers/api/NewsletterController.php
+++ travel_v1/app/Http/Controllers/api/NewsletterController.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Controllers\frontend;
+namespace App\Http\Controllers\api;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreNewsletter;
 use App\Newsletter;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class NewsletterController extends Controller
 {
